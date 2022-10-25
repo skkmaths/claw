c604ba73aa156394a5bbf9ea24375b0143da6bce
sudarshan <skkmaths@gmail.com>
2022-10-25 16:59:29 +0530

    add lw scheme

--- 2dscalar/sin2pi.py
+++ 2dscalar/sin2pi.py
@@ -1,8 +1,8 @@
 import numpy as np
 from constants import *
 
-xmin, xmax = 0.0, 1.0
-ymin, ymax = 0.0, 1.0
+xmin, xmax = -1.0, 1.0
+ymin, ymax = -1.0, 1.0
 
 lvbc, rvbc, bhbc, thbc = periodic, periodic, periodic, periodic
 
