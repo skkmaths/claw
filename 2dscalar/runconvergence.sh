87490675ad5dbdc0cf0d5b8df7e7a9925a03f38e
skkmaths <skkmaths@gmail.com>
2022-10-25 23:13:35 +0530

    converges files add

---
+++ 2dscalar/runconvergence.sh
@@ -0,0 +1,12 @@
+NCELL=$1
+FILE=error.txt
+rm -rf $FILE && touch $FILE
+for ncell in $NCELL
+do 
+   echo "ncell = $ncell"
+   python clw2d.py -Tf 2.0 -ncellx $ncell -ncelly $ncell -compute_error yes \
+          -plot_freq 0 -scheme lxw>log.txt
+   tail -n 1 log.txt
+   tail -n 1 log.txt >> $FILE
+done
+echo "Wrote file $FILE"
