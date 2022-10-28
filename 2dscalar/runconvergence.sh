00d97b1985bf3546c70a0ede9157b4cb9f977ddf
skkmaths <skkmaths@gmail.com>
2022-10-28 18:21:52 +0530

    conv

--- 2dscalar/runconvergence.sh
+++ 2dscalar/runconvergence.sh
@@ -5,7 +5,7 @@ for ncell in $NCELL
 do 
    echo "ncell = $ncell"
    python clw2d.py -Tf 2.0 -ncellx $ncell -ncelly $ncell -compute_error yes \
-          -plot_freq 0 -scheme lxw>log.txt
+          -plot_freq 0 -scheme lw>log.txt
    tail -n 1 log.txt
    tail -n 1 log.txt >> $FILE
 done
