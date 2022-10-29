4d3ca9600ee6442971a3224e5c255f45a2a2173a
skkmaths <skkmaths@gmail.com>
2022-10-29 21:16:22 +0530

    tmp.py

---
+++ 1dscalar/tmp.py
@@ -0,0 +1,216 @@
+"""
+Solve u_t + f(u)_x = 0  for f(u) 
+Finite volume scheme
+"""
+from xml import dom
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+from ic import *
+
+# Get arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-nc', type=int, help='Number of cells', default=100)
+parser.add_argument('-cfl', type=float, help='CFL number', default=0.9)
+parser.add_argument('-scheme',
+                    choices=('C','LF','GLF','LLF','LW','ROE','EROE','GOD'),
+                    help='Scheme', default='LF')
+parser.add_argument('-tvbM', type=float, help='TVB M parameter', default=0.0)
+parser.add_argument('-ic',
+                    choices=('smooth','shock','rare1','hat','rare','expo','slope'),
+                    help='Initial condition', default='smooth')
+parser.add_argument('-Tf', type=float, help='Final time', default=1.0)
+
+parser.add_argument('-pde', choices=('linear','varadv','burger','bucklev','concave',
+                                     'burger_adv',
+                                     'oreqn1','oreqn2','oreqn3','oreqn4'),
+                    help='PDE', default='linear')
+parser.add_argument('-numflux', help='Numerical Flux',choices=('rusanov','godunov','upwind'), default='rusanov')
+parser.add_argument('-compute_error', choices=('no','yes'),
+                    help='Compute error norm', default='no')
+parser.add_argument('-plot_freq', type=int, help='Frequency to plot solution',
+                    default=1)
+parser.add_argument('-time_scheme', default = 'euler',
+                    help = 'Chosen by degree if unspecified',
+                    choices = ('euler','ssprk22'))
+parser.add_argument('-limit', choices=('no', 'mmod'), help='Apply limiter',
+                    default='no')
+parser.add_argument('-alpha', type=float, help='Slope parameter', default=0.5)
+args = parser.parse_args()
+# Select PDE
+
+if args.pde == 'burger':
+    from burger import *
+elif args.pde == 'linear':
+    from linear import *
+elif args.pde == 'concave':
+    from concave import *
+
+# Select Numerical Flux
+if args.numflux not in numfluxes:
+    print("Incorrect numerical flux chosen, choices are ",numfluxes)
+    exit()
+if args.numflux == 'rusanov':
+    numflux = rusanov
+elif args.numflux == 'godunov':
+    numflux = godunov
+elif args.numflux == 'upwind':
+    numflux = upwind
+
+# constants
+Tf    = args.Tf
+cfl   = args.cfl
+uinit = args.ic
+nc    = args.nc
+alpha = args.alpha  # parameter in the slopes
+beta = 2.0 # parameter in minmod
+time_scheme = args.time_scheme
+
+if args.ic == 'smooth':
+    uinit = smooth
+elif args.ic == 'shock':
+    uinit = shock
+elif args.ic == 'hat':
+    uinit = hat
+
+xmin, xmax = 0.0, 1.0
+x   = np.zeros(nc)
+h = (xmax - xmin)/nc
+Mdx2 = args.tvbM*h**2.0
+  
+for i in range(nc):
+    x[i] = xmin + i*h + 0.5*h
+
+u = np.zeros(nc+4)    # with 4 ghost cells, 2 each sides 
+u[2:nc+2] = uinit(x)   # initialize solution variable
+res = np.zeros(nc+4)
+
+def minmod(a,b,c,Mdx2):
+    if np.abs(a) < Mdx2:
+        return a
+    sa = np.sign(a)
+    sb = np.sign(b)
+    sc = np.sign(c)
+    if sa==sb and sb==sc:
+        return sa * np.abs([a,b,c]).min()
+    else:
+        return 0.0
+    
+# plot initial condition
+if args.plot_freq >0:
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    line1,line2 = ax.plot(x, u[2:nc+2], 'ro',x, u[2:nc+2], 'b')
+    #line1, = ax.plot(x, u, 'o')
+    ax.set_xlabel('x'); ax.set_ylabel('u')
+    plt.title('nc='+str(nc)+', CFL='+str(cfl))
+    plt.legend(('Numerical','Exact'))
+    plt.grid(True); plt.draw(); plt.pause(0.1)
+    wait = input("Press enter to continue ")
+
+#Error computation
+def compute_error(u1,t):
+    error_norm1 = 0.0; error_norm2 = 0.0
+    ue = uexact(x, t , uinit)
+    dom_len = xmax - xmin
+    error_norm1 = h*np.sum(np.abs(u1-ue))
+    error_norm2 = np.sqrt(h*np.sum((u1-ue)**2))
+    return error_norm1, error_norm2
+
+def reconstruct(conjm1, conj, conjp1):
+    if args.limit == 'no':
+        return conj
+    elif args.limit == 'mmod':
+        dj = 2.0* alpha * minmod( beta*(conj-conjm1), \
+                                0.5*(conjp1-conjm1), \
+                                beta*(conjp1-conj), Mdx2 )
+        conl = conj + 0.5 * dj
+        return conl
+    else:
+        print('limit type not define')
+        exit()
+def update_ghost():
+    # left ghost cell
+    u[0] = u[nc]
+    u[1] = u[nc+1]
+
+    u[nc+3] = u[3]
+    u[nc+2] = u[2]
+
+# First order Euler forward step
+def apply_euler(t,lam, u_old, u, ures ):
+    #first stage
+    ts  = t
+    ures = compute_residual(ts, lam, u, ures)
+    u = u - lam * ures
+    return u
+
+def apply_ssprk22(t,lam, u_old, u, ures ):
+    #first stage
+    ts  = t
+    ures = compute_residual(ts, lam, u, ures)
+    u = u - lam * ures
+    
+    #second stage
+    ts = t + dt
+    ures = compute_residual(ts, lam, u, ures)
+    u = 0.5 * u_old + 0.5 *(u - lam * ures)
+    return u
+
+def compute_residual(ts, lam, u, res):
+    res[:] = 0.0    
+    # loop through all faces
+    update_ghost()
+    
+    for i in range(1,nc+2): # face between i and i+1
+        xf = xmin+(i-1)*h # location of the face
+        ul = reconstruct(u[i-1], u[i], u[i+1])
+        ur = reconstruct(u[i+2], u[i+1], u[i])
+        fl, fr = flux(xf,ul), flux(xf,ur)
+        fn = numflux(xf, ul, ur, fl, fr)
+        res[i] += fn
+        res[i+1] -= fn
+
+    return res
+
+time_schemes = { 'euler': apply_euler, 'ssprk22' : apply_ssprk22 }
+
+t, it = 0.0, 0
+while t < Tf:
+    if args.pde == 'linear':
+        dt= cfl * h
+    elif args.pde == 'burger':
+        dt= cfl * h /max_speed(u)
+    else:
+        print('dt is not set')
+        exit()
+        
+    lam = dt/h
+    if t+dt > Tf:
+        dt = Tf - t
+        lam = dt/h
+    
+    u_old = u
+    
+    u = time_schemes[time_scheme](t, lam, u_old, u, res)  # update solution
+
+    t += dt; it += 1 # update time step
+    if args.plot_freq >0:
+        ue = uexact(x, t , uinit)
+        line1.set_ydata(u[2:nc+2])
+        line2.set_ydata(ue)
+        plt.draw(); plt.pause(0.1)
+
+fname = 'sol.txt'
+np.savetxt(fname, np.column_stack([x, u[2:nc+2]]))
+print('Saved file ', fname)
+
+if args.compute_error == 'yes':
+    er1, er2 = compute_error(u[2:nc+2],t)
+    print('h, L1 error norm, L2 error norm = ')
+    print(h, er1, er2)
+if args.plot_freq >0:
+    plt.show()
+
+
+
