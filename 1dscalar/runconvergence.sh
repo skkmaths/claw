38867ff4cda2908927e579a3c412189b99070020
sudarshan <skkmaths@gmail.com>
2024-05-14 12:57:37 +0530

    edit runcongernce.sh

--- 1dscalar/runconvergence.sh
+++ 1dscalar/runconvergence.sh
@@ -4,7 +4,7 @@ rm -rf $FILE && touch $FILE
 for ncell in $NCELL
 do 
    echo "ncell = $ncell"
-   python clw.py -ic smooth -Tf 2.0 -cfl 0.9 -nc $ncell -pde linear -numflux rusanov  -compute_error yes -theta 0.5 \
+   python clw.py -ic smooth -Tf 2.0 -cfl 0.5 -nc $ncell -pde linear -numflux rusanov  -compute_error yes -limit mmod\
           -plot_freq 0 -time_scheme ssprk22>log.txt
    tail -n 1 log.txt
    tail -n 1 log.txt >> $FILE
