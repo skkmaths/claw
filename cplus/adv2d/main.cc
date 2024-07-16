fd6937931697f159e3c9450dd73dc4b248ec0c86
Sudarshan <sudarshan@Sudarshans-Mac-mini.local>
2024-07-16 17:47:41 +0530

    add parse

--- cplus/adv2d/main.cc
+++ cplus/adv2d/main.cc
@@ -13,9 +13,28 @@
 #include "twodproblem.h"
 
 using namespace std;
-int main() {
-
-    TwoDProblem twodproblem;
+int main(int argc, char* argv[]) {
+    int nx = 50; // Default value for nx
+    int ny = 50; // Default value for ny
+    double Tf = 2.0; // Default value for Tf
+    double cfl = 0.4; // Default value of cfl 
+    // Parse command-line arguments if provided
+    // Pass this argument to the exe file 
+    // ./twodproblem 50 50 2
+    if (argc > 1) {
+        nx = std::atoi(argv[1]);
+    }
+    if (argc > 2) {
+        ny = std::atoi(argv[2]);
+    }
+    if (argc > 3) {
+        Tf = std::atof(argv[3]);
+    }
+    if (argc > 4) {
+        cfl = std::atof(argv[4]);
+    }
+    
+    TwoDProblem twodproblem(nx, ny, Tf, cfl);
     twodproblem.run();
     return 0;
 }
