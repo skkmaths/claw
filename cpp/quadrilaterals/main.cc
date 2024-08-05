af80438d50716c85ed5b44c617846374dcadcb29
Sudarshan <sudarshan@Sudarshans-Mac-mini.local>
2024-08-05 12:37:11 +0530

    create .h file for visual

--- cpp/quadrilaterals/main.cc
+++ cpp/quadrilaterals/main.cc
@@ -8,77 +8,8 @@
 #include <sstream>
 #include <sys/stat.h>
 #include"grid.h"
+#include"vis.h"
 
-int fileid = 0;
-// For file name
-void createDirectory(const std::string& dirname) 
-{
-    struct stat info;
-    if(stat(dirname.c_str(), &info) != 0 || !(info.st_mode & S_IFDIR)) {
-        if(mkdir(dirname.c_str(), 0777) != 0) {
-            std::cerr << "Error creating directory " << dirname << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        std::cout << "Directory " << dirname << " is created" << std::endl;
-    }
-}
-std::string getFilename(const std::string& basename, int id) {
-    std::ostringstream oss;
-    oss << basename << "_" << std::setfill('0') << std::setw(4) << id << ".vtk";
-    return oss.str();
-}
-// Function to write initial condition to a VTK file
-// Function to write initial condition to a VTK file
-void savesol(const Mesh& mesh, std::vector<double>& solution, const double& t) {
-    std::string dirname = "sol";
-    createDirectory(dirname);
-    if(fileid == 0) {
-        std::cout << "The directory \"sol\" is going to be formatted!" << std::endl;
-        std::string pattern = "./sol/*";
-        // Remove existing files
-        system(("rm -f " + pattern).c_str());
-    }
-    std::string filename = getFilename("sol/sol", fileid);
-    std::ofstream file(filename);
-    file << "# vtk DataFile Version 3.0\n";
-    file << "Advection Solution at time " << t << "\n";
-    file << "ASCII\n";
-    file << "DATASET UNSTRUCTURED_GRID\n";
-
-    // Write points
-    file << "POINTS " << mesh.nodes.size() << " float\n";
-    for (const auto &node : mesh.nodes) {
-        file << node.x << " " << node.y << " " << node.z << "\n";
-    }
-
-    // Write cells
-    file << "CELLS " << mesh.cells.size() << " " << 5 * mesh.cells.size() << "\n";
-    for (const auto &cell : mesh.cells) {
-        file << "4 " << cell.nodes[0]->id << " " << cell.nodes[1]->id << " " << cell.nodes[2]->id << " " << cell.nodes[3]->id << "\n";
-    }
-
-    // Write cell types
-    file << "CELL_TYPES " << mesh.cells.size() << "\n";
-    for (std::size_t i = 0; i < mesh.cells.size(); ++i) {
-        file << "9\n"; // VTK_QUAD
-    }
-
-    // Write field data for time
-    file << "FIELD FieldData 1\n";
-    file << "TIME 1 1 float\n";
-    file << t << "\n";
-
-    // Write cell data
-    file << "CELL_DATA " << mesh.cells.size() << "\n";
-    file << "SCALARS sol float 1\n";
-    file << "LOOKUP_TABLE default\n";
-    for (const auto &value : solution) {
-        file << value << "\n";
-    }
-
-    file.close();
-    fileid++;
-}
 
 // Initial condition function
 double initialCondition(double x, double y) {
