79e69ee63145fb8a18af5f7ae8576370921604d9
Sudarshan <sudarshan@Sudarshans-Mac-mini.local>
2024-08-07 07:08:16 +0530

    major updates with var advection

--- cpp/quadrilaterals/main.cc
+++ cpp/quadrilaterals/main.cc
@@ -10,12 +10,42 @@
 #include"grid.h"
 #include"vis.h"
 
-
-// Initial condition function
-double initialCondition(double x, double y) {
-    double r = std::sqrt(pow(x - 0.2, 2) + pow(y - 0.2, 2));
-    if (r < 0.1) return 1.0;
+// Advection velocity
+Node velocity(const double& x, const  double& y)
+{
+    Node v;
+    v.x = y;
+    v.y = -x;
+    return v;
+}
+double initialCondition(const double& x,const  double& y){
+    std::string ic = "nonsmooth";
+    if (ic == "nonsmooth" ) // solid body rotation non smooth
+    {    double r = sqrt( pow(x+0.45,2)+pow(y,2));
+         if ( x> 0.1 & x<0.6 & y>-0.25 & y< 0.25  ) 
+         {
+            return 1.0;
+         }
+         else if ( r < 0.35)
+         {
+            return 1-r/0.35;
+         }
     else return 0.0;
+    }
+    // Smooth initial data
+    else if ( ic == "smooth") // solid body rotation
+    {   double r = sqrt( pow(x+0.3,2)+pow(y+0.3,2));
+        if ( r < 0.2){ return 1-r/0.2; }
+        else return 0.0;
+    }
+    else if( ic == "expo")
+    {
+        return exp(-100.0*( pow(x+0.3,2) + pow(y+0.3,2) ));
+    }
+    else 
+    {    std::cout<<" Unknown ic"<<std::endl;
+         abort();
+    }
 }
 // Initialize the solution
 void initialize(const std::vector<Cell> cells, std::vector<double>& solution) {
@@ -38,21 +68,15 @@ void compute_residue(const std::vector<double> &sol, std::vector<double> &res, c
     std::fill(res.begin(), res.end(), 0.0);
     for (const auto& face : mesh.faces) {
         if (!face.isBoundary) { // Check if the face is not a boundary
-            Node n = face.normalLeft; // Normal vector pointing towards right triangle
+            double flux;
+            Node n = face.normalLeft; // Outward unit normal to leftcell
             Cell* L = face.leftCell;
             Cell* R = face.rightCell;
-            //double lengthface = std::sqrt(std::pow(face.nodes[0]->x - face.nodes[1]->x, 2) + std::pow(face.nodes[0]->y - face.nodes[1]->y, 2));
-            double theta = std::acos(n.x); // Angle between right normal and positive x axis
-            if (n.y > 0.0) theta = std::acos(n.x);
-            else theta = 2.0 * M_PI - std::acos(n.x);
-            //double speed_xi = std::cos(theta) + std::sin(theta); // Speed in the xi direction of the transformed PDE
-            //double splus = std::max(speed_xi, 0.0);
-            //double sminus = std::min(speed_xi, 0.0);
-            double flux;
-            if (n.x + n.y > 0.0) flux = sol[L->id] * (n.x + n.y);
-            else flux = sol[R->id] * (n.x + n.y);
+            Node vel = velocity(face.midpoint.x, face.midpoint.y); // Advection velocity at face mid point
+            double velnormal = vel.x * n.x + vel.y * n.y;
             //double lam = std::max(L->perimeter/L->area, R->perimeter/R->area)*dt;
             //flux = 0.5*( (n.x + n.y)*(sol[L->id]+ sol[R->id])-(sol[R->id] -sol[L->id])/lam);
+            flux = (velnormal>0)? velnormal * sol[L->id] : velnormal * sol[R->id];
             res[L->id] += face.length * flux / L->area;
             res[R->id] -= face.length * flux / R->area;
         }
@@ -84,7 +108,7 @@ int main() {
         mesh.readFromGmsh("mesh.msh");
         double dt ;
         double time = 0.0;
-        double Tf = 0.5;
+        double Tf = 2.0* M_PI; // final time
         double cfl = 0.5;
         double speed = -1e-20;
         double dx = 0.01;
@@ -92,7 +116,7 @@ int main() {
         for(auto &cell : mesh.cells)
         speed = std::max( speed, 1.0/dx + 1.0/dy + 1e-14 );
         dt = cfl/speed;       
-        unsigned int save_freq = 1;
+        unsigned int save_freq = 10;
         unsigned int iter = 0;
         std::vector<double> solution, res, ue;
         res.resize(mesh.cells.size(),0.0);
