449c597cf1a7f5399b18ba7f311e34b4e07d6deb
Sudarshan <sudarshan@Sudarshans-Mac-mini.local>
2024-08-12 13:59:09 +0530

    add bdy conditions

--- cpp/quadrilaterals/main.cc
+++ cpp/quadrilaterals/main.cc
@@ -15,8 +15,8 @@ std::string flux_type = "lf";
 Node velocity(const double& x, const  double& y)
 {
     Node v;
-    v.x = y;
-    v.y = -x;
+    v.x = -y;
+    v.y = x;
     return v;
 }
 double initialCondition(const double& x,const  double& y){
@@ -35,7 +35,7 @@ double initialCondition(const double& x,const  double& y){
     }
     // Smooth initial data
     else if ( ic == "smooth") // solid body rotation
-    {   double r = sqrt( pow(x+0.3,2)+pow(y+0.3,2));
+    {   double r = sqrt( pow(x-0.3,2)+pow(y-0.3,2));
         if ( r < 0.2){ return 1-r/0.2; }
         else return 0.0;
     }
@@ -56,6 +56,10 @@ void initialize(const std::vector<Cell> cells, std::vector<double>& solution) {
         solution[i] = initialCondition(cell.centroid.x, cell.centroid.y);
     }
 }
+// Exact solution for c1 = y, c2 = -x, of ut+c1ux+c2uy = 0
+double exactvaradv(const Node& p, const double& t) {
+   return initialCondition( std::cos(t) * p.x + std::sin(t) * p.y, -std::sin(t)* p.x + std::cos(t) *p.y);
+}
 // exact the solution
 void exact(const std::vector<Cell> cells, std::vector<double>& ue, const double& t) {
     ue.resize(cells.size());
@@ -65,17 +69,18 @@ void exact(const std::vector<Cell> cells, std::vector<double>& ue, const double&
     }
 }
 // Compute residue
-void compute_residue(const std::vector<double> &sol, std::vector<double> &res, const Mesh &mesh, const double& dt) {
+void compute_residue(const std::vector<double> &sol, std::vector<double> &res,  Mesh &mesh, const double& dt, const double& time) {
     std::fill(res.begin(), res.end(), 0.0);
     for (const auto& face : mesh.faces) {
+        
+        double flux;
+        Node n = face.normalLeft; // Outward unit normal to leftcell
+        Cell* L = face.leftCell;
+        Cell* R = face.rightCell;
+        Node vel = velocity(face.midpoint.x, face.midpoint.y); // Advection velocity at face mid point
+        double velnormal = vel.x * n.x + vel.y * n.y;
+        double speed = std::abs(velnormal);
         if (!face.isBoundary) { // Check if the face is not a boundary
-            double flux;
-            Node n = face.normalLeft; // Outward unit normal to leftcell
-            Cell* L = face.leftCell;
-            Cell* R = face.rightCell;
-            Node vel = velocity(face.midpoint.x, face.midpoint.y); // Advection velocity at face mid point
-            double velnormal = vel.x * n.x + vel.y * n.y;
-            double speed = std::abs(velnormal);
             if ( flux_type == "lf") 
             flux = 0.5 * ( velnormal * (sol[L->id] + sol[R->id] ) - (sol[R->id] - sol[L->id])*speed); // Lax-Friedrich Flux
             else if( flux_type == "upwind")
@@ -84,6 +89,25 @@ void compute_residue(const std::vector<double> &sol, std::vector<double> &res, c
             res[L->id] += face.length * flux / L->area;
             res[R->id] -= face.length * flux / R->area;
         }
+        else if(face.isBoundary)
+        {   std::string facebdryid = mesh.get_boundary_id(face);
+            if(facebdryid == "left")
+            {
+                flux =  velnormal * exactvaradv(face.midpoint, time); 
+                res[L->id] += face.length * flux / L->area;
+            }
+            if(facebdryid == "bottom")
+            {
+                flux =  velnormal * exactvaradv(face.midpoint, time); 
+                res[L->id] += face.length * flux / L->area;
+            }
+        }
+        else 
+        {
+            std::cout<<"Face type is not known !"<<std::endl;
+             abort();
+        }
+
     }
 }
 // To find max of solution vector
@@ -114,8 +138,8 @@ int main() {
         std::cout<<"Reading mesh completed"<<std::endl;
         double dt ;
         double time = 0.0;
-        double Tf = 2.0* M_PI; // final time
-        double cfl = 0.5;
+        double Tf = M_PI/4; // final time
+        double cfl = 0.9;
         double speed = -1e-20;
         for(auto &cell : mesh.cells)
         {   Node vel = velocity(cell.centroid.x, cell.centroid.y);
@@ -132,7 +156,7 @@ int main() {
         while (time < Tf)
         {
             if (time + dt >Tf){ dt = Tf-time;}
-            compute_residue(solution, res, mesh, dt);
+            compute_residue(solution, res, mesh, dt, time);
             // update solution
             for (auto& cell : mesh.cells)
             {
@@ -142,20 +166,13 @@ int main() {
             time +=dt;
             iter +=1;
             if (save_freq > 0){       
-               //if (iter % save_freq == 0){savesol(time, sol);}
+                if (iter % save_freq == 0){savesol(mesh, solution, time);}
                 std::cout << std::left;
                 std::cout << "iter = " << std::setw(8) << iter 
                 << "time = " << std::setw(10) << time 
                 << "Max = " << std::setw(15) << solmax(solution)
                 << "Min = " << std::setw(15) << solmin(solution) << std::endl;
             }
-            
-            if( save_freq >0)
-               if ( iter % save_freq == 0) 
-               { 
-                   savesol(mesh, solution, time);
-               }
-            
         } 
         std::cout<<"Total number of cells = "<<mesh.cells.size()<<std::endl;
         savesol(mesh, solution, time);
